0fbd1f74cb309c1eac117bd5a16976ce892bdcfb
Barrie Treloar <baerrach@gmail.com>
2016-01-11 18:35:21 +1030

    Use melpa stable first and auto-install dash
    For people forking your files it doesn't work "out of the box" because you have stuff already installed.
    
    So first-timers run into more problems than needed.

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -1,14 +1,18 @@
 (require 'package)
-(require 'dash)
 
 ;; Add melpa to package repos
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
   (package-refresh-contents))
+
+(if (not (package-installed-p 'dash))
+    (package-install 'dash))
+
+(require 'dash)
 
 (defun packages-install (packages)
   (--each packages
