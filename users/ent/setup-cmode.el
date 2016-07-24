7ddb287575b941fa7832259f901ab313aa048989
Hojin Yoo <hjfirst@gmail.com>
2016-07-24 13:56:12 -0700

    add individual user settings

---
+++ users/ent/setup-cmode.el
@@ -0,0 +1,14 @@
+;; c/c++ mode indentation
+(defun my-c-mode-common-hook ()
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0) ;; if (cond) '\n' {
+  ;; other customizations can go here
+
+  (setq c++-tab-always-indent t)
+  ;;(setq c-basic-offset 4)                  ;; Default is 2
+  ;;(setq c-indent-level 4)                  ;; Default is 2
+
+  ;;(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  ;;(setq tab-width 4)
+  ;;(setq indent-tabs-mode t)  ; use spaces only if nil
+  (setq indent-tabs-mode nil))
