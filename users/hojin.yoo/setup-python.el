9039011c2c69b0eb23df8ec13a03be1bb99fd1fd
hojin.yoo <hojin.yoo@sentient.ai>
2016-08-02 13:34:06 -0700

    python settings for project "octave"

--- users/hojin.yoo/setup-python.el
+++ users/hojin.yoo/setup-python.el
@@ -1,3 +1,12 @@
+(add-hook 'python-mode-hook
+      (lambda ()
+        (setq indent-tabs-mode nil)
+        (setq tab-width 2)
+        (setq python-indent 2)
+        (setq py-autopep8-options '("--indent-size=2"))
+))
+
+(setenv "PYTHONPATH" "/Users/hojin.yoo/project/repo/scripts/_modules/python")
 (pyvenv-activate "~/project/python/")
 (elpy-use-ipython)
 
@@ -5,3 +14,11 @@
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; I added the following:
+;; cat ~/.config/flake8
+;; [flake8]
+;; ignore = E221,E501,E203,E202,E272,E251,E211,E222,E701
+;; max-line-length = 160
+;; exclude = tests/*
+;; max-complexity = 10
