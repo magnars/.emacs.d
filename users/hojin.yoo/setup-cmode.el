ba35dac17f2aebd7152146df2b5e24240cb7105e
hojin.yoo <hojin.yoo@sentient.ai>
2016-08-04 16:17:32 -0700

    modify c-mode

--- users/hojin.yoo/setup-cmode.el
+++ users/hojin.yoo/setup-cmode.el
@@ -1,14 +1,30 @@
 ;; c/c++ mode indentation
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (if (derived-mode-p 'c-mode 'c++-mode)
+                (cppcm-reload-all))))
+;; OPTIONAL, somebody reported that they can use this package with Fortran
+(add-hook 'c90-mode-hook (lambda () (cppcm-reload-all)))
+;; OPTIONAL, avoid typing full path when starting gdb
+(global-set-key (kbd "C-c C-g")
+                '(lambda ()(interactive)
+                   (gud-gdb (concat "gdb --fullname " (cppcm-get-exe-path-current-buffer)))))
+;; OPTIONAL, some users need specify extra flags forwarded to compiler
+;; (setq cppcm-extra-preprocss-flags-from-user '("-I/usr/src/linux/include" "-DNDEBUG"))
+
 (defun my-c-mode-common-hook ()
   ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
   (c-set-offset 'substatement-open 0) ;; if (cond) '\n' {
   ;; other customizations can go here
 
-  (setq c++-tab-always-indent t)
-  ;;(setq c-basic-offset 4)                  ;; Default is 2
-  ;;(setq c-indent-level 4)                  ;; Default is 2
+  ;; (setq c++-tab-always-indent nil)
+  (setq c-basic-offset 2)                  ;; Default is 2
+  (setq c-indent-level 2)                  ;; Default is 2
 
   ;;(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
   ;;(setq tab-width 4)
   ;;(setq indent-tabs-mode t)  ; use spaces only if nil
   (setq indent-tabs-mode nil))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
