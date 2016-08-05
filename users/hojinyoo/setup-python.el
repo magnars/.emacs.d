a6619f5f9d15f00e0ca895cd3827686ced4156ec
Hojin Yoo <hojin.yoo@home>
2016-08-04 17:37:39 -0700

    add desktop setup

---
+++ users/hojinyoo/setup-python.el
@@ -0,0 +1,7 @@
+(pyvenv-activate "~/project/python/")
+(elpy-use-ipython)
+
+;; use flycheck not flymake with elpy
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
