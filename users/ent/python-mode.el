68b097837e5b0cb3e742a5db926414fa7d06b166
hojin.yoo <hojin.yoo@sentient.ai>
2016-08-23 23:39:33 -0700

    remove ent

--- users/ent/python-mode.el
+++
@@ -1,9 +0,0 @@
-(require 'setup-python)
-
-(pyvenv-activate "~/project/venv/")
-(elpy-use-ipython)
-
-;; use flycheck not flymake with elpy
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
