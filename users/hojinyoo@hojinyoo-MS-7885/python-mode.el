40ffdfd1beab232195ee7a91c73cb0a1647e2255
hojinyoo <hjfirst@gmail.com>
2016-10-01 20:46:37 -0700

    add ob-ipython

--- users/hojinyoo@hojinyoo-MS-7885/python-mode.el
+++ users/hojinyoo@hojinyoo-MS-7885/python-mode.el
@@ -3,7 +3,9 @@
 (add-hook 'python-mode-hook
           (lambda ()
             (setq indent-tabs-mode nil)
-            (setq py-autopep8-options '("--max-line-length=120"))
+            (setq tab-width 2)
+            (setq python-indent 2)
+            (setq py-autopep8-options '("--indent-size=2 --max-line-length=120"))
             ;; (local-set-key (kbd "C-c C-d") 'python-restart--shell-send-region-or-buffer)
             ;; (defun python-restart--shell-send-region-or-buffer (&optional arg)
             ;;   "Restart the python shell, and send the active region or the buffer to it."
@@ -34,3 +36,22 @@
 ;; max-line-length = 160
 ;; exclude = tests/*
 ;; max-complexity = 10
+
+(use-package ob-ipython
+  :ensure t)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((ipython . t)
+   ;; other languages..
+   ))
+
+(defun my-ob-ipython-config ()
+  "For use in `org-mode-hook'."
+  (local-set-key (kbd "C-c C-o") 'org-edit-src-code)
+  (setq org-confirm-babel-evaluate nil)   ;don't prompt me to confirm everytime I want to evaluate a block
+  ;;; display/update images in the buffer after I evaluate
+  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
+)
+
+(add-hook 'org-mode-hook 'my-ob-ipython-config)
