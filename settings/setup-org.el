eece16a8eaf1ec89e6317166c140ae189a3cfe2d
hojinyoo <hjfirst@gmail.com>
2017-04-16 16:14:54 -0700

    jupyter and org-mode combination

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -11,31 +11,32 @@
 ;; (defadvice kill-whole-line (after fix-cookies activate)
 ;;   (myorg-update-parent-cookie))
 
-;; (setq org-directory "~/Dropbox/org")
+(setq org-directory "~/Dropbox/org")
 ;; (setq org-default-notes-file (concat org-directory "/notes.org"))
 ;; (define-key global-map (kbd "M-<f6>") 'org-capture)
 
 (setq org-log-done t)
 
-(setq org-agenda-files (list "/rp2:~/org/work.org"
-                             "/rp2:~/org/home.org"
-                             "/rp2:~/org/mobileorg.org"
-                             "/rp2:~/org/todo.org"
-                             ))
+;; (setq org-agenda-files (list "/rp2:~/org/work.org"
+;;                              "/rp2:~/org/home.org"
+;;                              "/rp2:~/org/mobileorg.org"
+;;                              "/rp2:~/org/todo.org"
+;;                              ))
 
 (setq org-todo-keywords
       '((sequence "TODO" "|" "DONE" "DELEGATED" "CANCELED")))
 
 (global-set-key (kbd "C-c c") 'org-capture)
 
-(setq org-default-notes-file "/rp2:~/org/todo.org")
+(setq org-default-notes-file "~/Dropbox/org/note.org")
 
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "/rp2:~/org/todo.org" "Tasks")
+      '(("t" "Todo" entry (file+headline "~/Dropbox/org/todo.org" "Tasks")
          "* TODO %?\n  %i\n  %a")
-        ("j" "Journal" entry (file+datetree "/rp2:~/org/home.org")
+        ("j" "Journal" entry (file+datetree "~/Dropbox/org/diary.org")
          "* %?\nEntered on %U\n  %i\n  %a")
-        ("w" "Todo" entry (file+headline "/rp2:~/org/work.org" "Work Tasks")
-         "* TODO %?\n  %i\n  %a")))
+        ;; ("j" "Journal" entry (file+datetree "~/Dropbox/org/diary.org")
+        ;;  "* Event: %?\n\n  %i\n\n  From: %a" :empty-lines 1)
+        ))
 
 (provide 'setup-org)
