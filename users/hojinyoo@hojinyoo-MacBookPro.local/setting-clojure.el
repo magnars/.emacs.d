ee56f7176269d186fa99a048d9ed41604b93bdbe
hojin yoo <hojin.yoo@mixpanel.com>
2017-10-26 11:05:25 -0700

    refactor clojure-mode

--- users/hojinyoo@hojinyoo-MacBookPro.local/setting-clojure.el
+++ users/hojinyoo@hojinyoo-MacBookPro.local/setting-clojure.el
@@ -1,40 +1,11 @@
-;; packages we want installed
-
-(use-package cider :ensure t)
-(use-package clojure-mode :ensure t)
-(use-package popup :ensure)
-(use-package company :ensure t)
-
-(use-package clojure-cheatsheet :ensure t)
+(require 'setup-clojure-mode)
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp/kubemacider")
 (require 'kubemacider)
 
-;; Cider &amp; nREPL
-(add-hook 'cider-mode-hook #'eldoc-mode)
-;; (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
-(setq nrepl-popup-stacktraces nil)
-(add-to-list 'same-window-buffer-names "<em>nrepl</em>")
-
-;; Disable moving to error buffer
-(setq cider-auto-select-error-buffer nil)
-
-;; company mode
-(add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
-(add-hook 'cider-repl-mode-hook #'toggle-truncate-lines)
-(add-hook 'cider-stacktrace-mode-hook #'toggle-truncate-lines)
-(add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
-;; (global-set-key (kbd "TAB") #'company-indent-or-complete-common)
-
-;; Pretty print results in repl
-(setq cider-repl-use-pretty-printing t)
-
-;; yasnippet
-(use-package clojure-snippets :ensure t)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;; clojure mode hook and helpers
+
 (defun clojure-mode-custom-indent ()
   (put-clojure-indent 'fnk 'defun)
   (put-clojure-indent 'defnk 'defun)
@@ -58,9 +29,6 @@
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max)))
 
-(defun my-tab-fix ()
-  (local-set-key [tab] 'company-indent-or-complete-common))
-
 (add-hook 'clojure-mode-hook
           #'(lambda ()
               (clojure-mode-custom-indent)
@@ -74,43 +42,8 @@
               (add-hook 'before-save-hook 'indent-whole-buffer nil t)
               (add-hook 'before-save-hook 'delete-trailing-whitespace)))
 
-(add-hook 'cider-repl-mode-hook
-          #'(lambda () (my-tab-fix)))
-
 ;; let cider use the monorepo
 (setq cider-lein-parameters "monolith with-all :select :default repl :headless :host ::")
 ; (setq cider-lein-parameters "monolith with-all :select :default with-profile dev repl :headless :host ::")
 
-;; show nrepl server port
-(setq nrepl-buffer-name-show-port t)
-
-;; cider test mode - show report
-(setq cider-test-show-report-on-success t)
-
-;; retain history
-(setq cider-repl-wrap-history t)
-(setq cider-repl-history-size 10000)
-(setq cider-repl-history-file (concat (getenv "HOME") "/.emacs.d/cider-history"))
-
-(use-package fill-column-indicator
-  :ensure t
-  :config
-  (setq fci-rule-column 100)
-  (setq fci-rule-width 1)
-  (add-hook 'clojure-mode-hook 'fci-mode))
-
-(use-package clojure-mode-extra-font-locking :ensure t)
-
-(add-hook 'clojure-mode-hook #'subword-mode)
-(add-hook 'clojure-mode-hook #'turn-on-smartparens-strict-mode)
-
-(use-package rainbow-delimiters
-  :ensure t
-  :config (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode))
-(use-package rainbow-mode :ensure t)
-
 (cider-add-to-alist 'cider-jack-in-lein-plugins "cider/cider-nrepl" (upcase "0.15.1"))
-
-(use-package ob-clojure
-  :ensure t
-  :config (setq org-babel-clojure-backend 'cider))
