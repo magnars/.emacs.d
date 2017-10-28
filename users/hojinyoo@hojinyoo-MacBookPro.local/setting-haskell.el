95afee4ecf6fb009d0ec1e9d53aa526c379bb78f
hojin yoo <hojin.yoo@mixpanel.com>
2017-10-27 18:05:30 -0700

    haskell-mode

--- users/hojinyoo@hojinyoo-MacBookPro.local/setting-haskell.el
+++ users/hojinyoo@hojinyoo-MacBookPro.local/setting-haskell.el
@@ -1,15 +1,9 @@
-(use-package haskell-mode
-  :ensure t)
-
+(use-package haskell-mode :ensure t)
 ;; (use-package scion :ensure t)
+(use-package hindent :ensure t)
+(use-package intero :ensure t)
 
-(use-package hindent
-  :ensure t)
-
-(use-package intero
-  :ensure t)
-
-;; (add-hook 'haskell-mode-hook 'intero-mode)
+(add-hook 'haskell-mode-hook 'intero-mode)
 
 ;; See https://github.com/serras/emacs-haskell-tutorial/blob/master/tutorial.md
 (add-hook 'haskell-mode-hook #'hindent-mode)
@@ -26,7 +20,6 @@
   (setenv "PATH" (concat my-cabal-path path-separator (getenv "PATH")))
   (add-to-list 'exec-path my-cabal-path))
 
-
 ;; use setq properly instead of custom-set-variables
 (custom-set-variables '(haskell-tags-on-save t))
 
@@ -36,3 +29,4 @@
   '(haskell-process-log t))
 
 (custom-set-variables '(haskell-process-type 'cabal-repl))
+(custom-set-variables '(haskell-interactive-popup-errors nil))
